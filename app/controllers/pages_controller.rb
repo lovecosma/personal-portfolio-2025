daa3e060295cfea345f344a5dbb62fda8204428c
Matthew Williams <williamsmatthew333@gmail.com>
2026-02-02 12:29:54 -0700

    abstract urls to constants

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+SAMPLE_WEBSITE_URL = "https://sol-calculations-2a5543766a23.herokuapp.com/".freeze
+RESUME_PATH = "/docs/MatthewWilliamsResume.pdf".freeze
 	def home
 
 	end
