8ee37b991b605df95d040782e687a3e33c20084d
Matthew Williams <williamsmatthew333@gmail.com>
2026-02-02 12:59:06 -0700

    mend

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 SAMPLE_WEBSITE_URL = "https://sol-calculations-2a5543766a23.herokuapp.com/".freeze
-RESUME_PATH = "/docs/MatthewWilliamsResume.pdf".freeze
+RESUME_PATH = "/docs/MatthewWilliamsResumev2.pdf".freeze
 	def home
 
 	end
