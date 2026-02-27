240ed33ccc8f86ce65e687178dfe9b1c72840db6
Matthew Williams <williamsmatthew333@gmail.com>
2026-02-26 23:06:14 -0700

    makes responsive experience uniform across pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,16 @@
 class PagesController < ApplicationController
 	def home
+		expires_in 24.hours, public: true
 		@tech_types = TechType.ordered.includes(:technologies)
 		@sample_website_url = Rails.application.config.x.sample_website_url
 		@resume_path = Rails.application.config.x.resume_path
 	end
 
 	def about
-
+		expires_in 24.hours, public: true
 	end
 
 	def contact
-
+		expires_in 24.hours, public: true
 	end
 end
