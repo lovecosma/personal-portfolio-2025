74c67c0a92c989db9d2a910f0cef300bf226f5ab
Matthew Williams <williamsmatthew333@gmail.com>
2026-03-16 18:00:55 -0400

    adds remaining subsections to engineering philosophy with styling and svgs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,16 @@ module ApplicationHelper
     content_for(:description) || "Full-stack software engineer with 4+ years of experience building web applications with Ruby on Rails. Based in Colorado Springs, CO."
   end
 
+  def inline_icon(filename, **attrs)
+    path = Rails.root.join("app/assets/images/icons/#{filename}")
+    svg = File.read(path)
+    if attrs.any?
+      attr_string = attrs.map { |k, v| "#{k}=\"#{v}\"" }.join(" ")
+      svg = svg.sub("<svg", "<svg #{attr_string}")
+    end
+    svg.html_safe
+  end
+
   def page_keywords
     content_for(:keywords) || "Matthew Williams, Software Engineer, Ruby on Rails, Full Stack Developer, Web Development, Colorado Springs, Rails Developer"
   end
